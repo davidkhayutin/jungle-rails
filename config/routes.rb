be045b2a1863705bad8a8add99bf91b97226bab4
David <david.khayutin@gmail.com>
2018-11-11 20:08:30 +0000

    user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
+
 Rails.application.routes.draw do
 
   root to: 'products#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
+    put    :add_item
+    delete :remove_item
   end
 
   resources :orders, only: [:create, :show]
